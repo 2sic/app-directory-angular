b6f68a2d32791e7b6a87d1a14f544ae66200f587
raphir <rhicode@gmail.com>
2017-05-11 09:51:47 +0200

    working directory module

---
+++ src/app/directory/directory.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DirectoryComponent } from './directory.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    DirectoryComponent
+  ],
+  declarations: [
+    DirectoryComponent
+  ]
+})
+export class DirectoryModule { }
