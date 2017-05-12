13431520cc0ceec499b16da762d4cf9f35e19feb
raphir <rhicode@gmail.com>
2017-05-12 13:46:08 +0200

    added departments to service. display departments in dropdown.

--- src/app/directory/directory.service.ts
+++ src/app/directory/directory.service.ts
@@ -5,31 +5,42 @@ import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
 
 import { DirectoryEntry } from "app/directory/directory-entry";
+import { Department } from "app/directory/department";
 
 @Injectable()
 export class DirectoryService {
   entries: Observable<DirectoryEntry[]>;
+  departments : Observable<Department[]>;
   base: string = 'http://agv-sw2017.60.2sic.net';
+  path: string = '/DesktopModules/2sxc/API/app/auto/content';
 
   private headers: Headers = new Headers();
 
   private entrySubject: Subject<DirectoryEntry[]> = new Subject<DirectoryEntry[]>();
+  private departmentSubject: Subject<Department[]> = new Subject<Department[]>();
 
   constructor(
     private http: Http
   ) {
+    this.departments = this.departmentSubject.asObservable();
     this.entries = this.entrySubject.asObservable();
     this.headers.append('ModuleId', '408');
     this.headers.append('TabId', '73');
-    this.getItems();
+    this.getDirectoryItems();
+    this.getDeparmentEntries();
   }
   
-  private getItems(): void {
-    this.http.get(`${this.base}/DesktopModules/2sxc/API/app/auto/content/DirectoryItem`, { headers: this.headers })
+  private getDirectoryItems(): void {
+    this.http.get(`${this.base + this.path}/DirectoryItem`, { headers: this.headers })
       .map(res => res.json().map((entry: DirectoryEntry) => {
         entry.Logo = this.base + entry.Logo;
         return entry;
       }))
-      .subscribe(res => this.entrySubject.next(res));
+      .subscribe(entries => this.entrySubject.next(entries));
+  }
+
+  private getDeparmentEntries(): void {
+    this.http.get(`${this.base + this.path}/Department`, { headers: this.headers })
+    .subscribe(res => this.departmentSubject.next(res.json()));
   }
 }
