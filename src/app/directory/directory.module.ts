32a4d87533626ab95eca7e51376e0045ca6a1f6f
Christoph Bühler <christoph.buehler@hotmail.com>
2017-10-31 13:36:53 +0100

    local changes

--- src/app/directory/directory.module.ts
+++ src/app/directory/directory.module.ts
@@ -3,7 +3,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SelectModule } from 'angular2-select';
- 
+
 import { DirectoryComponent } from './directory.component';
 import { DirectoryData } from 'app/directory/directory-data.service';
 import { Data, ContentManagerModule } from '@2sic.com/dnn-sxc-angular';
